1d4ba76e1422467a1156046ae9b9b4239fa9fbd2
Marcus Betti <marcusbetti@gmail.com>
2024-11-20 14:55:02 -0300

    inclusao de projetos

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,6 +8,10 @@ import { Modal, Button } from 'react-bootstrap';
 import api_Chamados from '../assets/api_Chamados.png';
 import COVID_proj from '../assets/COVID_proj.png';
 import desafio_proj from '../assets/desafio_proj.png';
+// import minhasSenhasApp from '../assets/minhasSenhasApp.png';
+// import API_4 from '../assets/API_4.png';
+// import API_3 from '../assets/API_3.png';
+import logo_port from '../assets/logo_port.jpg';
 // Adicione mais imports de imagens conforme necessário
 
 interface Project {
@@ -53,7 +57,48 @@ const Projects: React.FC = () => {
       tecnologias: [faReact, faNodeJs, faPython, faHtml5, faCss3Alt],
       githubLink: "https://github.com/marcusvbe/Site-Dev-Web-1.-semestre",
     },
-    // Adicione mais projetos conforme necessário
+    {
+      title: "Aplicativo Android de Gerenciamento de Senhas – Ago ‑ Dez 2024",
+      // image: minhasSenhasApp,
+      image: logo_port,
+      shortDescription: "Desenvolvimento de aplicativo Android para gerenciamento seguro de senhas com autenticação avançada.",
+      detalhes: `Na disciplina de Programação para Dispositivos Móveis I, escolhemos o tema Gerenciamento de Senhas para o desenvolvimento de aplicativo
+      Android em dupla. Criptografia de ponta a ponta de todos os dados do usuário. Autenticação de 2 fatores: login e biometria.`,
+      tecnologias: [faReact, faNodeJs, faFigma, faJs, faHtml5, faCss3Alt],
+      githubLink: "https://github.com/minhasSenhasApp2024/minhasSenhasApp",
+    },
+    {
+      title: "Sistema de Coleta de Dados de Estações Meteorológicas – Ago ‑ Dez 2024",
+      // image: API_4,
+      image: logo_port,
+      shortDescription: "Desenvolvimento de sistema para coleta e monitoramento de dados meteorológicos em tempo real.",
+      detalhes: `No projeto integrador em grupo (API) deste semestre (disciplinas de DevOps e IoT), desenvolvemos um sistema para coletar dados de
+      sensores (direção e velocidade do vento, índice pluviométrico, umidade, temperatura e pressão), tratá‑los e exibi‑los em um portal com dashboards
+      e relatórios. Geração de alertas: funcionalidade central do portal, ferramenta de monitoramento ambiental para lidar com desastres
+      naturais. Recurso didático: a demonstração dos conceitos matemáticos usados nos cálculos dos parâmetros.`,
+      tecnologias: [faReact, faNodeJs, faPython, faHtml5, faCss3Alt, faJava, faJira],
+      githubLink: "https://github.com/equipe-javali/API_4",
+    },
+    {
+      title: "Sistema de Gestão de Ativos para Empresas – Fev ‑ Jul 2024",
+      // image: API_3,
+      image: logo_port,
+      shortDescription: "Desenvolvimento de sistema para gestão eficiente de ativos empresariais, tanto tangíveis quanto intangíveis.",
+      detalhes: `No API do 3º semestre, nossa missão foi desenvolver um sistema para gestão de ativos, tangíveis e intangíveis.`,
+      tecnologias: [faReact, faJava, faNodeJs, faHtml5, faCss3Alt, faGithub],
+      githubLink: "https://github.com/equipe-javali/API_3",
+    },
+    {
+      title: "Sistema de Gestão de Chamados (API) 02/2023",
+      image: api_Chamados,
+      shortDescription: "Desenvolvimento de API para gestão de chamados direcionada a instituições de ensino.",
+      detalhes: `O projeto integrado do 2º semestre de 2023 consistiu na criação de uma aplicação de gestão de chamados direcionada a escolas e instituições
+      de ensino. Minha participação esteve mais concentrada no back-end, desenvolvendo a API, que incluiu a criação de rotas, operações de CRUD,
+      e a lógica de funcionamento do sistema, como as mudanças de status do chamado refletindo ações do cliente, do técnico e do admin.
+      Também fiz integrações com o front-end.`,
+      tecnologias: [faReact, faNodeJs, faPython, faHtml5, faCss3Alt, faJs, faJira, faSlack],
+      githubLink: "https://github.com/marcusvbe/FocusQuestAPI",
+    },
   ];
 
   return (
@@ -86,15 +131,14 @@ const Projects: React.FC = () => {
               <p><strong>Tecnologias utilizadas:</strong></p>
               <div className="tecnologias">
                 {selectedProject.tecnologias.map((icon, idx) => (
-                  typeof icon === 'object' ? (
-                    <FontAwesomeIcon key={idx} icon={icon} style={{ fontSize: '24px', marginRight: '10px' }} />
-                  ) : (
-                    <FontAwesomeIcon key={idx} icon={faReact} style={{ fontSize: '24px', marginRight: '10px' }} /> // Ajuste conforme necessário
-                  )
+                  <FontAwesomeIcon key={idx} icon={icon} style={{ fontSize: '24px', marginRight: '10px' }} />
                 ))}
               </div>
               <p><strong>Contribuições pessoais:</strong></p>
-              <p>Desenvolvimento do front-end em React usando TypeScript, HTML e CSS</p>
+              <p>
+                {/* Você pode personalizar melhor esta parte para cada projeto, se necessário */}
+                Desenvolvimento do front-end em React usando TypeScript, HTML e CSS.
+              </p>
               {/* Adicione mais informações conforme necessário */}
             </>
           )}
