bf4b0e741b072cbe542b4a612c8ec443a326fc2f
Marcus Betti <marcusbetti@gmail.com>
2024-11-21 20:17:06 -0300

    alteracoes na secao detalhes dos projetos

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -48,17 +48,17 @@ const Projects: React.FC = () => {
       modalTitle: "Minhas Senhas",
       image: minhasSenhasApp,
       shortDescription: "Aplicativo de gerenciamento de senhas com biometria e criptografia de ponta a ponta – 2º semestre de 2024",
-      detalhes: `Na disciplina de Programação para Dispositivos Móveis I, escolhemos o tema Gerenciamento de Senhas para o desenvolvimento de aplicativo
-Android em dupla. Criptografia de ponta a ponta de todos os dados do usuário. Autenticação de 2 fatores: login e biometria.`,
+      detalhes: `Na disciplina de Programação para Dispositivos Móveis I, escolhemos o tema Gerenciamento de Senhas para o desenvolvimento de aplicativo Android em dupla. 
+      Criptografia de ponta a ponta de todos os dados do usuário. Autenticação de 2 fatores: login e biometria.`,
       hardSkills: [
+        "criptografia AES‑256 com chave mestra",
+        "expo secure store: armazenamento seguro das credenciais do usuário e chave mestra no dispositivo",
         "Typescript e Node.js em todo o projeto",
         "Framework Expo",
-        "expo secure store: armazenamento seguro das credenciais do usuário e chave mestra no dispositivo",
         "Firebase: autenticação do usuário com e‑mail e senha",
         "Firestore para armazenamento NoSQL dos dados criptografados dos usuários",
-        "criptografia AES‑256 com chave mestra"
       ],
-      softSkills: ["Trabalho em Equipe", "Gerenciamento de Tempo", "Resolução de Problemas"],
+      softSkills: ["Organização", "Priorização de tarefas", "Resolução de Problemas"],
       contribuicoes: ["Desenvolvi toda a estrutura do projeto, configurei o Firebase, Firestore e todos os seus serviços e sua integração ao código, toda a lógica de funcionamento do app, estrutura de navegação, autenticação e criptografia."],
       githubLink: "https://github.com/minhasSenhasApp2024/minhasSenhasApp",
     },
@@ -68,13 +68,15 @@ Android em dupla. Criptografia de ponta a ponta de todos os dados do usuário. A
       modalTitle: "Sistema de Coleta de Dados de Estações Meteorológicas",
       // image: API_4,
       image: desafio_proj,
-      detalhes: `No projeto integrador em grupo (API) deste semestre (disciplinas de DevOps e IoT), desenvolvemos um sistema para coletar dados de
-      sensores (direção e velocidade do vento, índice pluviométrico, umidade, temperatura e pressão), tratá‑los e exibi‑los em um portal com dashboards
-      e relatórios. Geração de alertas: ferramenta de monitoramento ambiental para lidar com desastres naturais. Recurso didático: a demonstração dos conceitos matemáticos usados nos cálculos dos parâmetros. Habilidades interpessoais nos APIs: a cada API são fornecidos requisitos funcionais e não funcionais, cabendo ao grupo a priorização (MVP), definição das entregas a cada sprint, sua validação com o cliente e a gestão do projeto utilizando a metodologia ágil SCRUM.`,
-      hardSkills: ["Typescript, Node.js: backend", "Swagger: documentação de APIs", "AWS, Vercel: hospedagem e deploy automático", "Github Actions: pipeline de integração contínua", "React e Typescript: front‑end", "PostgreSQL: banco de dados relacional", "Neon: banco de dados relacional on-line", "Redis: banco de dados em memória", "Metodologia Ágil SCRUM: gestão do projeto"],
+      detalhes: `Os APIs (Aprendizagem por Projeto Integrado) são projetos semestrais em grupo desenvolvidos com a metodologia ágil SCRUM. A cada API são fornecidos requisitos funcionais e não funcionais, cabendo ao grupo a priorização (MVP), definição das entregas a cada sprint, sua validação com o cliente e a gestão da documentação.
+
+      No API deste semestre (disciplinas de DevOps e IoT), desenvolvemos um sistema para coletar dados de sensores (direção e velocidade do vento, índice pluviométrico, umidade, temperatura e pressão), tratá‑los e exibi‑los em um portal com dashboards e relatórios. 
+      Geração de alertas: ferramenta de monitoramento ambiental para lidar com desastres naturais. 
+      Recurso didático: a demonstração dos conceitos matemáticos usados nos cálculos dos parâmetros. `,
+      hardSkills: ["JWT: autenticação e autorização", "bcrypt: hashing de senhas", "Typescript, Node.js: backend", "Swagger: documentação de APIs", "AWS, Vercel: hospedagem e deploy automático", "Github Actions: pipeline de integração contínua", "React e Typescript: front‑end", "PostgreSQL, Neon: banco de dados relacional", "Redis: banco de dados em memória", "Metodologia Ágil SCRUM: gestão do projeto"],
       softSkills: ["Gestão de tempo, proatividade, trabalho em equipe"],
       contribuicoes: [
-        "Autenticação e Autorização:  Desenvolvi funcionalidades de CRUD de usuários, sistema de login e proteção de rotas utilizando bcrypt para hashing de senhas e JWT para geração de tokens de acesso. Integração Contínua: Desenvolvi pipelines de Integração Contínua (CI) e implementei seus scripts com GitHub Actions para integrar testes e deploys automatizados. Documentação de APIs: Utilizei Swagger para documentar as rotas relativas a usuários e login, facilitando a comunicação e integração entre as equipes de front-end e back-end."
+        "Autenticação e Autorização:  Desenvolvi funcionalidades de CRUD de usuários, sistema de login e proteção de rotas utilizando bcrypt para hashing de senhas e JWT para geração de tokens de acesso.", "Integração Contínua: Desenvolvi pipelines de Integração Contínua (CI) e implementei seus scripts com GitHub Actions para integrar testes e deploys automatizados.", "Documentação de APIs: Utilizei Swagger para documentar as rotas relativas a usuários e login, facilitando a comunicação e integração entre as equipes de front-end e back-end."
       ],
       githubLink: "https://github.com/equipe-javali/API_4",
     },
@@ -87,7 +89,7 @@ Android em dupla. Criptografia de ponta a ponta de todos os dados do usuário. A
       detalhes: `Criação de uma aplicação web para o monitoramento dos ativos (tangíveis e intangíveis) de uma empresa, com informações sobre o valor atual do ativo, onde ele está no momento, seu responsável, manutenções, entre outros`,
       hardSkills: ["Java, framework Spring: backend", "React e Typescript: frontend", "PostgreSQL: banco de dados relacional", "Supabase: banco de dados relacional on‑line", "Jira: gestão de tarefas", "Metodologia Ágil SCRUM: gestão do projeto"],
       softSkills: ["Aprimoramento de processo: seguir um padrão na criação de branches e commits no Github", "Trabalho em Equipe", "Gerenciamento de Tempo", "Resolução de Problemas"],
-      contribuicoes: ["Criei o banco de dados físico, a partir da modelagem que recebi. API rest: criei os endpoints e operações de CRUD. Fiz a integração do backend com o banco de dados."],
+      contribuicoes: ["Criei o banco de dados físico, a partir da modelagem que recebi.", "API rest: criei os endpoints e operações de CRUD.", "Fiz a integração do backend com o banco de dados."],
       githubLink: "https://github.com/equipe-javali/API_3",
     },
     {
@@ -95,10 +97,12 @@ Android em dupla. Criptografia de ponta a ponta de todos os dados do usuário. A
       shortDescription: "Sistema de gestão de chamados de TI direcionado a instituições de ensino – 2º semestre de 2023",
       modalTitle: "Sistema de Gestão de Chamados",
       image: api_Chamados,
-      detalhes: `O projeto integrado do 2º semestre de 2023 consistiu na criação de uma aplicação de gestão de chamados de TI. Nossa equipe optou por desenvolvê-la direcionada a escolas e instituições de ensino para a gestão de chamados relativos a problemas de software. O sistema tem 3 níveis de acesso diferentes: cliente, técnico e administrador.`,
+      detalhes: `O projeto integrado do 2º semestre de 2023 consistiu na criação de uma aplicação de gestão de chamados de TI. 
+      Nossa equipe optou por desenvolvê-la direcionada a escolas e instituições de ensino para a gestão de chamados relativos a problemas de software. 
+      O sistema tem 3 níveis de acesso diferentes: cliente, técnico e administrador.`,
       hardSkills: ["Node.js e Typescript: backend", "Javascript e React: frontend", "SQLite: banco de dados relacional", "Prisma: ORM", "GitHub: controle de versão com uso de submódulos", "Metodologia Ágil SCRUM: gestão do projeto"],
       softSkills: ["Trabalho em Equipe", "Gerenciamento de Tempo", "Resolução de Problemas"],
-      contribuicoes: ["Minha participação esteve mais concentrada no backend, desenvolvendo a API, que incluiu a criação de rotas, operações de CRUD, e a lógica de funcionamento do sistema, como as mudanças de status do chamado refletindo ações do cliente, do técnico e do admin. Também fiz integrações com o frontend."],
+      contribuicoes: ["Desenvolvi a API rest: criação de rotas e operações de CRUD", "Implementei a lógica de funcionamento do sistema, como as mudanças de status do chamado refletindo ações do cliente, do técnico e do admin.", "Fiz integrações com o frontend."],
       githubLink: "https://github.com/marcusvbe/FocusQuestAPI",
     },
     {
@@ -106,7 +110,8 @@ Android em dupla. Criptografia de ponta a ponta de todos os dados do usuário. A
       shortDescription: "Sistema para pesquisa de dados sobre a Covid longa – 1º semestre de 2023",
       modalTitle: "Sistema para pesquisa de dados sobre a Covid longa",
       image: COVID_proj,
-      detalhes: `Projeto do API (Aprendizagem por Projeto Integrado) do 1º semestre do curso de Desenvolvimento de Software Multiplataforma da FATEC de São José dos Campos – SP, em parceria com a TV Vanguarda. O site permite analisar e comparar alguns dados de saúde do sistema Datasus (Tabnet) a fim de identificar possíveis impactos da Covid longa em 3 cidades do Vale do Paraíba: São José dos Campos, Jacareí e Taubaté.`,
+      detalhes: `Projeto do API (Aprendizagem por Projeto Integrado) do 1º semestre do curso de Desenvolvimento de Software Multiplataforma da FATEC de São José dos Campos – SP, em parceria com a TV Vanguarda. 
+      O site permite analisar e comparar alguns dados de saúde do sistema Datasus (Tabnet) a fim de identificar possíveis impactos da Covid longa em 3 cidades do Vale do Paraíba: São José dos Campos, Jacareí e Taubaté.`,
       hardSkills: ["Figma: wireframe", "GitHub: controle de versão", "Python, Flask: backend", "MySQL: banco de dados", "HTML, CSS, Bootstrap: frontend", "Metodologia Ágil SCRUM: gestão do projeto"],
       softSkills: ["Trabalhar em equipe", "Gerenciar o tempo", "Falar em público nas apresentações das sprints do projeto"],
       contribuicoes: ["Usei o Figma para criação de páginas do wireframe do projeto.", "Contribuí com a implementação do site com Flask.", "Redigi seções do README do projeto no GitHub.", "Redigi textos do site."],
@@ -156,7 +161,7 @@ Android em dupla. Criptografia de ponta a ponta de todos os dados do usuário. A
           {selectedProject && (
             <>
               <h4>{selectedProject.modalTitle}</h4>
-              <p>{selectedProject.detalhes}</p>
+              <p className="project-details">{selectedProject.detalhes}</p>
 
               {/* Seção de Hard Skills e Soft Skills */}
               <div className="skills-section">
